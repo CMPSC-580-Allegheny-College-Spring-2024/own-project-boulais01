9059935f61f77efe61c60c562e1a02ebe73f7f1e
boulais01 <bookdragon1202@gmail.com>
2024-04-14 22:49:48 -0500

    feat: initialize main

--- src/mousetracker/mousetracker/main.py
+++ src/mousetracker/mousetracker/main.py
@@ -1,3 +1,15 @@
 """In which to build the prototype of a mouse tracking tool using pygame"""
 
 import pygame
+import typer
+
+from rich.console import Console
+
+cli = typer.Typer()
+
+console = Console()
+
+
+@cli.command()
+def main():
+    """Run mouse tracker with some demo game probably."""
