4d07d3fcaf77193e96b50f195a9991967e6b5546
boulais01 <bookdragon1202@gmail.com>
2024-04-20 15:16:40 -0500

    chore: add comments

--- src/mousetracker/mousetracker/main.py
+++ src/mousetracker/mousetracker/main.py
@@ -1,5 +1,10 @@
 """In which to build the prototype of a mouse tracking tool using pygame"""
 
+# data we're looking for:
+# choice, time to choice, mouse location at each point in time
+# perhaps a dictionary;
+# {"choice_point_or_id": {"choice": [(1sec, point), (2sec, point), ..., (Lastsec, point)]}}
+
 import pygame
 import typer
 
@@ -13,3 +18,8 @@ console = Console()
 @cli.command()
 def main():
     """Run mouse tracker with some demo game probably."""
+    # TODO: Establish calls outline
+    # TODO: fix doc string
+    # idea: make a simple two choice thing
+    # run it here - by passing into tool main function?
+    # display output neatly
