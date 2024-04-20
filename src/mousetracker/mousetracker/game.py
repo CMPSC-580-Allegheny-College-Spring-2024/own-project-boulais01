74481846559b6431ae9f3e5f263c2ff268762ef5
boulais01 <bookdragon1202@gmail.com>
2024-04-20 15:47:51 -0500

    feat: add files

---
+++ src/mousetracker/mousetracker/game.py
@@ -0,0 +1,3 @@
+"""A simple game to display tracker functionality."""
+
+# TODO: implement a simple pygame wih 2-3 choices
---
+++ src/mousetracker/mousetracker/tracker.py
@@ -0,0 +1,4 @@
+"""The main functionality of the mouse tracker."""
+
+# TODO: create mouse tracker
+# reference main for expected return
