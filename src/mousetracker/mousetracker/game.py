309c7b2a58eadb53b36dc725c678146e9e37b10b
boulais01 <bookdragon1202@gmail.com>
2024-05-03 15:10:05 -0500

    feat: outline

--- src/mousetracker/mousetracker/game.py
+++ src/mousetracker/mousetracker/game.py
@@ -8,6 +8,7 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None,50)
 running = True
 
 while running:
@@ -21,9 +22,24 @@ while running:
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
+    question = font.render("What's your favorite color?", True, (0,0,0))
 
+    answer1 = font.render("Red", True, (255,0,0))
+    answer2 = font.render("Blue", True, (0,0,255))
+    answer3 = font.render("Green", True, (0,255,0))
+
+    screen.blit(question, (360,0))
+
+    screen.blit(answer1, (540,100))
+    screen.blit(answer2, (540,200))
+    screen.blit(answer3, (540,300))
     # flip() the display to put your work on screen
     pygame.display.flip()
+
+    for event in pygame.event.get():
+        button1 = pygame.mouse.get_pressed(num_buttons=3)[0]
+        #if button1 is True:
+
 
     clock.tick(60)  # limits FPS to 60
 
